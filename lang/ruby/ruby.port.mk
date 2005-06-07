b40e19cfcce84c58e03aa8d61522d4053306ab82
taka <taka>
2005-06-07 00:57:03 +0000

    add ruby-1.8.2. not tested with FLAVOR=tk

---
+++ lang/ruby/ruby.port.mk
@@ -0,0 +1,22 @@
+# $OpenBSD: ruby.port.mk,v 1.1 2004/08/09 19:54:20 xsa Exp $
+
+# ruby module
+
+RUN_DEPENDS+=::lang/ruby
+LIB_DEPENDS+=ruby.1.82::lang/ruby
+
+# location of ruby libraries
+MODRUBY_LIBDIR=		${LOCALBASE}/lib/ruby
+
+# common directories for ruby extensions
+# used to create docs and examples install path
+MODRUBY_DOCDIR=		${PREFIX}/share/doc/ruby
+MODRUBY_EXAMPLEDIR=	${PREFIX}/share/examples/ruby
+
+CONFIGURE_STYLE?=	simple
+CONFIGURE_SCRIPT?=	${LOCALBASE}/bin/ruby extconf.rb
+
+REV=1.8
+SUB=${MACHINE_ARCH:S/amd64/x86_64/}-openbsd${OSREV}
+SUBST_VARS=SUB REV
+
