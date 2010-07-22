7e0baba7eae7ffc50f2de76c4f649848a9307b26
TAKAHASHI Kaoru <kaoru@kaisei.org>
2010-07-22 08:03:14 +0000

    vmh.c get along with OpenBSD 4.6 & OpenBSD 4.7.

--- mail/mh-plus/patches/patch-uip:vmh.c
+++ mail/mh-plus/patches/patch-uip:vmh.c
@@ -1,18 +1,11 @@
 --- uip/vmh.c.orig	Thu Jul 22 14:20:14 2010
 +++ uip/vmh.c	Thu Jul 22 14:20:33 2010
-@@ -116,7 +116,6 @@
+@@ -116,7 +116,7 @@
  
  #define	XYZ			/* XXX */
  
 -#if (defined(__FreeBSD__) && __FreeBSD__ >= 5) || (defined(linux))
++#if (defined(__OpenBSD__) && __NCURSES_VERSION_MINOR__ >= 6)
  struct ldat
  {
  	NCURSES_CH_T	*text;
-@@ -124,7 +123,6 @@
- 	NCURSES_SIZE_T	lastchar;
- 	NCURSES_SIZE_T	oldindex;
- };
--#endif
- 
- /*  */
- 
