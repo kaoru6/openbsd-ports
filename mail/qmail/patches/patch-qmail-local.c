bfe41dfa5c9f5ddccb5e66c2397b39d909e67832
TAKAHASHI Kaoru <kaoru@kaisei.org>
2010-08-05 15:28:29 +0000

    Fix PATCH_LIST.

--- mail/qmail/patches/patch-qmail-local.c
+++ mail/qmail/patches/patch-qmail-local.c
@@ -1,6 +1,5 @@
-diff -u qmail-1.03.old/qmail-local.c qmail-1.03/qmail-local.c
---- qmail-1.03.old/qmail-local.c	1998-06-15 05:52:55.000000000 -0500
-+++ qmail-1.03/qmail-local.c	2003-01-09 14:22:48.000000000 -0600
+--- qmail-local.c.old	1998-06-15 05:52:55.000000000 -0500
++++ qmail-local.c	2003-01-09 14:22:48.000000000 -0600
 @@ -645,7 +645,7 @@
      {
       cmds.s[j] = 0;
